ad8b1006c7be74939571d0fcccf1b40b3303ffc8
Punnapak <pupunbolt@gmail.com>
2023-06-08 12:03:32 +0700

    feat:weather-box

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './component/homepage/homepage.component';
+import { WeatherBoxComponent } from './component/weather-box/weather-box/weather-box.component';
+import { CommonModule } from '@angular/common';
+
+// Remaining code...
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    WeatherBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/component/homepage/homepage.component.ts
+++ my-app/src/app/component/homepage/homepage.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  dateTime: Date | any;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dateTime = new Date();
+  }
+
+  weatherData = [
+    { title: 'Weather Box 1', temperature: '25°C', condition: 'Sunny', boxStyles: { background: '#FF0000' }, imageSrc: '../assets/Sunny.svg' },
+    { title: 'Weather Box 2', temperature: '20°C', condition: 'Cloudy', boxStyles: { background: '#00FF00' }, imageSrc: 'path/to/image2.jpg' },
+    { title: 'Weather Box 3', temperature: '15°C', condition: 'Rainy', boxStyles: { background: '#0000FF' }, imageSrc: 'path/to/image3.jpg' }
+  ];
 }
