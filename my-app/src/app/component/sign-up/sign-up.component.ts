7d977129f8e84276cd5aae542cf1674c24530f77
Punnapak <pupunbolt@gmail.com>
2023-06-12 14:30:57 +0700

    fix: responsive

--- my-app/src/app/component/sign-up/sign-up.component.ts
+++ my-app/src/app/component/sign-up/sign-up.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
 
+
+type UnPromise<T> = T extends Promise<infer X>? X : T;
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,12 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
   loadingAnimationClass = 'loadingpic';
+  profile: UnPromise<ReturnType<typeof liff.getProfile>> | undefined;
   // Get the class from the css first 
+
 
   ngOnInit(): void {
     setTimeout(()=>{
       this.loadingAnimationClass= '';
     },4500)
+
+
+      liff.init({
+        liffId:'1661398192-oglAdg54'
+      }).then(() =>{
+    
+      if(liff.isLoggedIn()){
+        liff.getProfile().then( profile =>{
+          this.profile = profile;
+          console.log(this.profile)
+        }).catch(console.error);
+      }else{
+        liff.login()
+      }
+
+      }).catch((err)=>{
+        console.log(err.code, err.message)
+      })
+    
 
   }
 
